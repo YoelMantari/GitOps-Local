2740580e114c1c2ca6b01dfdca66933bb68a62e1
YoelMantari <yoel.mantari.s@uni.pe>
2025-06-16 21:56:20 -0500

    feat(verificar): validar existencia de servicio_dummy_B

--- tools/verificar_estado.py
+++ tools/verificar_estado.py
@@ -18,6 +18,7 @@ def validar_servicio_a() -> str:
     f = servicio_dir / "servicio_dummy_A.service"
     return "ok" if f.exists() else f"No existe {f}"
 
+
 def validar_servicio_b() -> str:
 
     f = servicio_dir / "servicio_dummy_B.service"
@@ -25,15 +26,3 @@ def validar_servicio_b() -> str:
     return "ok" if f.exists() else f"No existe {f}"
 
 
-def validar_servicio_c() -> str:
-
-    f = raiz_proyecto / "terraform" / "servicio_c" / "servicios" / "db_dummy.txt"
-    return "ok" if f.exists() else f"No existe {f}"
-
-
-def validar_servicio_d() -> str:
-    try:
-        with socket.create_connection((cola_host, cola_port), timeout=1):
-            return "ok"
-    except Exception as e:
-        return f"La cola no responde en {cola_host}:{cola_port} ({e})"
