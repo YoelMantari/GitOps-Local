95b91619223161f57b19bb1040abcdbeff09e12d
Dirac <daronmitchel@gmail.com>
2025-06-17 00:46:51 -0500

    feat(script): Mejora cola_dummy parametrizada
    Se modifico script cola_dummy.py para que reciba
    los argumentos de IP y Puerto y se agrega una cola
    que guarda los mensajes y una opcion para imprimir
    los mensajes (GET)

--- terraform/servicio_d/main.tf
+++ terraform/servicio_d/main.tf
@@ -15,12 +15,24 @@ module "servicio_b" {
   source = "../servicio_b"
 }
 
+variable "ip_cola_dummy" {
+  description = "El numero IP del socket donde se iniciara el proceso de escucha"
+  type        = string
+  default     = "127.0.0.1"
+}
+
+variable "puerto_cola_dummy" {
+  description = "El numero de puerto del socket donde se iniciara el proceso de escucha"
+  type        = number
+  default     = 1234
+}
+
 
 resource "null_resource" "servicio_d" {
 
   depends_on = [module.servicio_b]
 
   provisioner "local-exec" {
-    command = "nohup ${var.python_exec} ${local.ruta_raiz_proyecto}/scripts/cola_dummy.py > /tmp/cola_dummy.log 2>&1 &"
+    command = "nohup ${var.python_exec} ${local.ruta_raiz_proyecto}/scripts/cola_dummy.py ${var.ip_cola_dummy} ${var.puerto_cola_dummy} > /tmp/cola_dummy.log 2>&1 &"
   }
 }
